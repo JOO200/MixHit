0a7f5fc8bc3077caf536a0f18073f3099b7238e6
Shep-pard <soeren.alrutz@me.com>
2018-11-13 13:58:58 +0100

    config fixed + scale added
    config file i2c fix
    scale calibration changed

--- MixHit/src/libs/cScale.cpp
+++ MixHit/src/libs/cScale.cpp
@@ -2,37 +2,30 @@
 
 cScale::cScale()
 {
-	mTare = 96.57;
+	mTare = 643,4368;
 }
 cScale::cScale(byte pPinScaleData, byte pPinScaleCLK)
 {
 	mHX711Scale = HX711(pPinScaleData, pPinScaleCLK);
 	//mHX711Scale.set_scale(1);
-	mTare = 96.57;
+	mTare = 650.3619;
 }
 double cScale::getWeight()
 {
-	// Folgende Werte wurden durch Messen ermittelt.
-	//		Gewicht (G)		 digitaler Wert (DW)
-	// 1:	0g			=	 123463
-	// 2:	100g		=    252298
-	// Linearer Zusammenhang: G = m * DW + b
-	// m = (G1 - G2) / (DW1 - DW2) = (0 - 100) / (123463 - 252298) = 0,000776185631313688 = 1/1288;
-	// b = G1 - m * DW1 = 0 - 0,000776185631313688 * 123463 = -95,83
-
-	return (double)mHX711Scale.get_units(10) / 1288 - mTare; // Cast als Double, damit das Zwischenergebniss nicht gerundet wird.
+	
+ 
+	return (double)mHX711Scale.get_units(10) * 0.009551 - mTare; // Cast als Double, damit das Zwischenergebniss nicht gerundet wird.
+  //return (double)mHX711Scale.get_units(10); // Cast als Double, damit das Zwischenergebniss nicht gerundet wird.
+  
+ 
 }
 double cScale::getWeight(int pPoints)
 {
-	// Folgende Werte wurden durch Messen ermittelt.
-	//		Gewicht (G)		 digitaler Wert (DW)
-	// 1:	0g			=	 123463
-	// 2:	100g		=    252298
-	// Linearer Zusammenhang: G = m * DW + b
-	// m = (G1 - G2) / (DW1 - DW2) = (0 - 100) / (123463 - 252298) = 0,000776185631313688 = 1/1288;
-	// b = G1 - m * DW1 = 0 - 0,000776185631313688 * 123463 = -95,83
-
-	return (double)mHX711Scale.get_units(pPoints) / 1288 - mTare; // Cast als Double, damit das Zwischenergebniss nicht gerundet wird.
+	
+ 
+	return (double)mHX711Scale.get_units(pPoints) * 0.009551 - mTare; // Cast als Double, damit das Zwischenergebniss nicht gerundet wird.
+ 
+ 
 }
 void cScale::Tare()
 {
@@ -40,3 +33,25 @@ void cScale::Tare()
 	mTare = getWeight();
 }
 
+ 
+
+  // Folgende Werte wurden durch Messen ermittelt: Mit 5 V Spannungsversorgung HX711
+  //    Gewicht (G)    digitaler Wert (DW)
+  // 1: 0g      =  134800
+  // 2: 100g    =  155750 -->Prüfgewicht
+  // Linearer Zusammenhang: G = m * DW + b
+  // m = (G1 - G2) / (DW1 - DW2) = (0 - 100) / (134800 - 155750) = 0,0047733 ;
+  // b = G1 - m * DW1 = 0 - 0,0047733 * 134800= - 643,4368
+
+
+
+
+
+
+//Folgende Werte wurden durch Messen ermittelt: Mit 3,3 V Spannungsversorgung HX711
+  //    Gewicht (G)    digitaler Wert (DW)
+  // 1: 0g      =  61330
+  // 2: 100g    =  71800 -->Prüfgewicht
+  // Linearer Zusammenhang: G = m * DW + b
+  // m = (G1 - G2) / (DW1 - DW2) = (0 - 100) / (61330 - 71800) =0,009551 ;
+  // b = G1 - m * DW1 = 0 - 0,009551 * 61330= - 585,77
